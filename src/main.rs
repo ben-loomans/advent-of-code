059e020d4151fabf9e5ebd7c0c2a07a3255e941b
Ben Loomans <benjaminloomans@gmail.com>
2024-01-10 13:15:06 +1000

    set up day 18

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod twenty_twenty_three;
 use advent_of_code::solved::Solved;
-use crate::twenty_twenty_three::day_17::Solution;
+use crate::twenty_twenty_three::day_18::Solution;
 
 fn main() {
-    let solution = Solution::new("src/twenty_twenty_three/input/day_17_test.txt");
+    let solution = Solution::new("src/twenty_twenty_three/input/day_18.txt");
     solution.part_one();
     //solution.part_two();
 }
